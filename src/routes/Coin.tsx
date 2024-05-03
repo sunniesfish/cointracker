f78231a0f9b6e7423df42255cbcd0b93fe4fcdbc
sunniesfish <hhch5611.jh@gmail.com>
2024-05-03 17:51:42 +0900

    #5.7 Nested Routes part One (10:04)/(12:19)

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { Route, Switch, useLocation, useParams } from "react-router-dom";
 import { styled } from "styled-components";
 
 interface RouteParams{
@@ -70,6 +70,28 @@ interface IPriceData{
         }
     };
 }
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
 
 const Container = styled.div`
     padding: 0px 20px;
@@ -109,16 +131,50 @@ function Coin(){
         })();
         setLoading(false);
         console.log(priceInfo);
-    },[]);
+    },[coinId]);
     return (
         <Container>
             <Header>
-                <Title>{state?.name||"Loading..."}</Title>
+                <Title>{state?.name ? state.name : loading? "Loading...": info?.name}</Title>
             </Header>
             {loading? 
             <Loader>Loading...</Loader>
             :
-                null
+            <>
+            <Overview>
+              <OverviewItem>
+                <span>Rank:</span>
+                <span>{info?.rank}</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>Symbol:</span>
+                <span>${info?.symbol}</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>Open Source:</span>
+                <span>{info?.open_source ? "Yes" : "No"}</span>
+              </OverviewItem>
+            </Overview>
+            <Description>{info?.description}</Description>
+            <Overview>
+              <OverviewItem>
+                <span>Total Suply:</span>
+                <span>{priceInfo?.total_supply}</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>Max Supply:</span>
+                <span>{priceInfo?.max_supply}</span>
+              </OverviewItem>
+            </Overview>
+            <Switch>
+                <Route>
+                    <Chart/>
+                </Route>
+                <Route>
+                    <Price/>
+                </Route>
+            </Switch>
+          </>
             }
         </Container>
     );
