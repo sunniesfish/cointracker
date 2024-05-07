a4ba81fa61e7d2b6e5db66fccc9c65a4d3139ef9
sunniesfish <hhch5611.jh@gmail.com>
2024-05-07 12:28:26 +0900

    #5.11 React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Reset } from "./GlobalStyle";
 import Router from "./Router";
+import { ReactQueryDevtools} from "react-query/devtools";
 
 function App() {
   return (
     <>
     <Reset/>
     <Router/>
+    <ReactQueryDevtools/>
     </>
   );
 }
