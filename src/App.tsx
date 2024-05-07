e449da2fcb4def8a74311a114d86cc73cf204d91
sunniesfish <hhch5611.jh@gmail.com>
2024-05-07 17:40:54 +0900

    #6.0 Dark Mode part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import { Reset } from "./GlobalStyle";
 import Router from "./Router";
 import { ReactQueryDevtools} from "react-query/devtools";
-
+import { ThemeProvider } from 'styled-components';
+import { darkTheme,lightTheme } from "./theme";
+import { useState } from "react";
 
 function App() {
+  const [isDark, setIsDark] = useState(false);
+  const toggleDark = () => {setIsDark(current => !current)};
   return (
     <>
-    <Reset/>
-    <Router/>
-    <ReactQueryDevtools/>
+    <ThemeProvider theme={isDark? darkTheme : lightTheme}>
+      <button onClick={toggleDark}>Toggle Mode</button>
+      <Reset/>
+      <Router/>
+      <ReactQueryDevtools/>
+    </ThemeProvider>
     </>
   );
 }
