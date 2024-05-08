087c4665963c4711c976a70d8632f12f39e7711b
sunniesfish <hhch5611.jh@gmail.com>
2024-05-08 12:49:59 +0900

    making price tab

--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,9 +1,19 @@
+import { useQuery } from "react-query";
+import { fetchCoinTickers } from "../api";
+
 interface IPriceProps {
     coinId:string;
 }
 
+
+
 function Price({coinId}:IPriceProps){
-    
+    console.log(fetchCoinTickers(coinId))
+    // const [isLoading, data] = useQuery<>(
+    //     ["ticker",coinId],
+    //     () => fetchCoinTickers(coinId)
+    // );
+    // console.log(data)
     return(
         <div>
 
